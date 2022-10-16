fbb77c82c51d378e8e272964af6de32df7ed1469
Sajan Poudel <35656849+sajanpoudel@users.noreply.github.com>
2022-10-16 10:10:51 -0500

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -20,9 +20,7 @@ InputDecorationTheme inputDecorationTheme() {
     gapPadding: 10,
   );
   return InputDecorationTheme(
-    // If  you are using latest version of flutter then lable text and hint text shown like this
-    // if you r using flutter less then 1.20.* then maybe this is not working properly
-    // if we are define our floatingLabelBehavior in our theme then it's not applayed
+  
     floatingLabelBehavior: FloatingLabelBehavior.always,
     contentPadding: const EdgeInsets.symmetric(horizontal: 42, vertical: 20),
     enabledBorder: outlineInputBorder,
